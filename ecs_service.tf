95cc2a8327a58a622f303e8c03317509fceaf56b
Diego Rondon <rondiego@amazon.com>
2021-01-08 16:36:06 -0300

    checkpiont

--- ecs_service.tf
+++ ecs_service.tf
@@ -17,38 +17,39 @@ resource "aws_ecs_service" "ragazzid_app" {
   load_balancer {
     target_group_arn = aws_lb_target_group.app.arn
     container_name   = "web"
-    container_port   = 80
+    container_port   = 5000
   }
 
   network_configuration {
-    subnets         = module.vpc.public_subnets
+    subnets         = module.vpc.private_subnets
     security_groups = [module.vpc.default_security_group_id]
   }
 }
-#
-# resource "aws_ecs_service" "monitor" {
-#   name            = "${var.name_prefix}_monitor"
-#   cluster         = aws_ecs_cluster.monitor.id
-#   task_definition = aws_ecs_task_definition.monitor.arn
-#   desired_count   = 1
-#
-#   deployment_controller {
-#     type = "ECS"
-#   }
-#
-#   capacity_provider_strategy {
-#     base              = 0
-#     capacity_provider = aws_ecs_capacity_provider.monitor.name
-#     weight            = 1
-#   }
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.monitor.arn
-#     container_name   = "prometheus"
-#     container_port   = 9090
-#   }
-#
-#   network_configuration {
-#     subnets         = module.vpc.private_subnets
-#     security_groups = [module.vpc.default_security_group_id]
-#   }
-# }
+
+
+resource "aws_ecs_service" "monitor" {
+  name            = "${var.name_prefix}_monitor"
+  cluster         = aws_ecs_cluster.monitor.id
+  task_definition = aws_ecs_task_definition.monitor.arn
+  desired_count   = 1
+
+  deployment_controller {
+    type = "ECS"
+  }
+
+  capacity_provider_strategy {
+    base              = 0
+    capacity_provider = aws_ecs_capacity_provider.monitor.name
+    weight            = 1
+  }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.monitor.arn
+    container_name   = "prometheus"
+    container_port   = 9090
+  }
+
+  network_configuration {
+    subnets         = module.vpc.private_subnets
+    security_groups = [module.vpc.default_security_group_id]
+  }
+}
--- lb.tf
+++ lb.tf
@@ -19,6 +19,13 @@ resource "aws_lb_target_group" "app" {
   vpc_id      = module.vpc.vpc_id
   target_type = "ip"
 
+  health_check {
+    interval = 30
+    path     = "/actuator/health"
+    port     = 5000
+    matcher  = "200-299"
+  }
+
   depends_on = [
     aws_lb.this
   ]
@@ -35,21 +42,54 @@ resource "aws_lb_listener" "app" {
   }
 }
 
-# resource "aws_lb_target_group" "monitor" {
-#   name        = "${var.name_prefix}-lb-tg-monitor"
-#   port        = 9090
-#   protocol    = "HTTP"
-#   vpc_id      = module.vpc.vpc_id
-#   target_type = "ip"
-# }
-#
-# resource "aws_lb_listener" "monitor" {
-#   load_balancer_arn = aws_lb.this.arn
-#   port              = "9090"
-#   protocol          = "HTTP"
-#
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.monitor.arn
-#   }
-# }
+resource "aws_lb_target_group" "monitor" {
+  name        = "${var.name_prefix}-lb-tg-monitor"
+  port        = 9090
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
+  target_type = "ip"
+
+  health_check {
+    interval = 30
+    path     = "/-/healthy"
+    port     = 9090
+    matcher  = "200-299"
+  }
+}
+
+resource "aws_lb_listener" "monitor" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = "9090"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.monitor.arn
+  }
+}
+
+resource "aws_lb_target_group" "grafana" {
+  name        = "${var.name_prefix}-lb-tg-grafana"
+  port        = 3000
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
+  target_type = "ip"
+  #
+  # health_check {
+  #   interval = 30
+  #   path     = "/-/healthy"
+  #   port     = 9090
+  #   matcher  = "200-299"
+  # }
+}
+
+resource "aws_lb_listener" "grafana" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = "3000"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.monitor.arn
+  }
+}
